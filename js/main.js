7b2e69c2af7bcad2de15b7ac498f546bc4c37cfb
Jorge2712 <jorgeps2712@gmail.com>
2025-10-24 15:20:09 -0500

    combinando los js de los compañeros

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
 // main.js
-import { mostrar151 } from "./mostrar151.js";
+/* import { mostrar151 } from "./mostrar151.js";
 
 // Ejecutar al cargar la página
 document.addEventListener("DOMContentLoaded", () => {
   mostrar151();
 });
+ */
+
+// main.js
+import { cargarPokemons } from "./mostrar151.js";
+import { filtrarPokemons } from "./david.js";
+
+// ✅ Iniciar app
+cargarPokemons();
+
+// ✅ Conectar los filtros de búsqueda y tipo
+document.getElementById("searchPokemon").addEventListener("input", filtrarPokemons);
+document.getElementById("filterType").addEventListener("change", filtrarPokemons);
